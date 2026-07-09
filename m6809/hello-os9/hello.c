21db6ee35b888fd517b24e016d50eba36150a76d
Boisy Pitre <bpitre@apple.com>
2026-07-09 06:09:11 -0500

    Move C hello world examples to c-notebook

--- m6809/hello-os9/hello.c
+++
@@ -1,6 +0,0 @@
-#include <os9.h>
-
-int main(void) {
-    I$Write(1, "Hello, world!\n", 14);
-    return 0;
-}
--- m6809/hello/hello.c
+++
@@ -1,8 +0,0 @@
-int putchar(int c);
-
-int main(void) {
-    const char *msg = "HELLO, WORLD!\r";
-    while (*msg)
-        putchar(*msg++);
-    return 0;
-}
